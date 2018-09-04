a1f3f450896df12c806db578cefeeecb2fb8602c
andrei <andyahont@gmail.com>
2018-09-04 15:49:37 +0600

    API added models(Student, Course), admin view

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(verbose_name='Course Title', max_length=255)
+    description = models.TextField(verbose_name='Description')
+    img = models.ImageField(verbose_name='Picture', blank=True, null=True, upload_to='courses')
+    student = models.ManyToManyField('Student', related_name='student', blank=True, null=True)
+    is_deleted = models.BooleanField(verbose_name='Deleted', default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Student(models.Model):
+    first_name = models.CharField(verbose_name='First name', max_length=255)
+    last_name = models.CharField(verbose_name='Last name', max_length=255)
+    email = models.EmailField(verbose_name="Email")
+    phone_number = models.CharField(verbose_name='Phone number', max_length=100, blank=True, null=True)
+    id_number = models.CharField(verbose_name='Id number', max_length=25)
+    courses = models.ManyToManyField('Course', related_name='courses_title', blank=True, null=True)
+    img = models.ImageField(verbose_name='Picture', blank=True, null=True, upload_to='students')
+    is_deleted = models.BooleanField(verbose_name='Deleted', default=False)
+
+    def __str__(self):
+        return "Student - %s %s" % (self.first_name, self.last_name)
+
